d52fdd30b1b3b2e164930be00a5b7f8e39a2f0c9
RomanG <grotrk@gmail.com>
2021-10-14 23:25:17 +0300

    new pro

--- src/com/pb/gnat/gnat.java
+++ src/com/pb/gnat/gnat.java
@@ -2,6 +2,6 @@ package com.pb.gnat;
 
 public class gnat {
     public static void main(String[] args) {
-        System.out.println("Gnat+Maryna");
+        System.out.println("Гнат Гавяк");
     }
 }
